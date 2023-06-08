4423f2ecd6193ad31b6b5a09bf253ee56f01b6c9
Breno-lisboa-Souza <brenolisboa2004@gmail.com>
2023-06-07 21:41:13 -0300

    criar notas

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace PAM_MAUI_App.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+
+	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
